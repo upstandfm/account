a1c647f59fd922515e7c3d5053cb9ef4bc5522c1
Daniël Illouz <daniel.illouz@me.com>
2020-02-11 16:36:19 +0100

    Create worker to invite user

---
+++ src/workers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const schema = require('./schema');
+const { parseRecordBody } = require('./utils');
+
+module.exports = {
+  /**
+   * Invite a user.
+   *
+   * @param {Object} service - Invite service
+   * @param {Object} eventRecord - SQS event (FIFO Queue)
+   *
+   * @return {Promise} Void
+   */
+  inviteUser(service, eventRecord) {
+    if (!service) {
+      throw new Error('Provide a service to invite a user');
+    }
+
+    const record = schema.validateRecord(eventRecord);
+    const body = parseRecordBody(record.body);
+    const userInfo = schema.validateUserInfo(body);
+    return service.createInvite(userInfo);
+  }
+};
